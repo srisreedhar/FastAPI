e75c04ed2a6954cd8506c1cb5478ea5fa409fcac
srisreedhar <srisreedhar@live.com>
2025-07-26 17:06:16 +0530

    added superheroes data

---
+++ Basic/data/superheros.py
@@ -0,0 +1,182 @@
+superheroes = {
+    "Superman": {
+        "real_name": "Clark Kent",
+        "powers": ["Flight", "Super Strength", "X-Ray Vision", "Heat Vision"],
+        "team": "Justice League",
+        "home": "Metropolis"
+    },
+    "Batman": {
+        "real_name": "Bruce Wayne",
+        "powers": ["Genius Intellect", "Martial Arts", "Gadgets"],
+        "team": "Justice League",
+        "home": "Gotham City"
+    },
+    "Wonder Woman": {
+        "real_name": "Diana Prince",
+        "powers": ["Super Strength", "Flight", "Lasso of Truth"],
+        "team": "Justice League",
+        "home": "Themyscira"
+    },
+    "Spider-Man": {
+        "real_name": "Peter Parker",
+        "powers": ["Web-Slinging", "Wall-Crawling", "Spider-Sense"],
+        "team": "Avengers",
+        "home": "New York City"
+    },
+    "Iron Man": {
+        "real_name": "Tony Stark",
+        "powers": ["Powered Armor", "Genius Intellect", "Flight"],
+        "team": "Avengers",
+        "home": "New York City"
+    },
+    "Captain America": {
+        "real_name": "Steve Rogers",
+        "powers": ["Super Strength", "Shield Mastery", "Enhanced Agility"],
+        "team": "Avengers",
+        "home": "Brooklyn"
+    },
+    "Thor": {
+        "real_name": "Thor Odinson",
+        "powers": ["God of Thunder", "Mjolnir", "Super Strength"],
+        "team": "Avengers",
+        "home": "Asgard"
+    },
+    "Hulk": {
+        "real_name": "Bruce Banner",
+        "powers": ["Super Strength", "Regeneration", "Gamma Radiation"],
+        "team": "Avengers",
+        "home": "New Mexico"
+    },
+    "Black Widow": {
+        "real_name": "Natasha Romanoff",
+        "powers": ["Espionage", "Martial Arts", "Agility"],
+        "team": "Avengers",
+        "home": "Russia"
+    },
+    "Hawkeye": {
+        "real_name": "Clint Barton",
+        "powers": ["Archery", "Martial Arts", "Accuracy"],
+        "team": "Avengers",
+        "home": "Iowa"
+    },
+    "The Flash": {
+        "real_name": "Barry Allen",
+        "powers": ["Super Speed", "Time Travel", "Vibration"],
+        "team": "Justice League",
+        "home": "Central City"
+    },
+    "Green Lantern": {
+        "real_name": "Hal Jordan",
+        "powers": ["Power Ring", "Flight", "Constructs"],
+        "team": "Justice League",
+        "home": "Coast City"
+    },
+    "Aquaman": {
+        "real_name": "Arthur Curry",
+        "powers": ["Underwater Breathing", "Super Strength", "Telepathy"],
+        "team": "Justice League",
+        "home": "Atlantis"
+    },
+    "Black Panther": {
+        "real_name": "T'Challa",
+        "powers": ["Enhanced Abilities", "Vibranium Suit", "Leadership"],
+        "team": "Avengers",
+        "home": "Wakanda"
+    },
+    "Doctor Strange": {
+        "real_name": "Stephen Strange",
+        "powers": ["Magic", "Time Manipulation", "Teleportation"],
+        "team": "Avengers",
+        "home": "New York City"
+    },
+    "Captain Marvel": {
+        "real_name": "Carol Danvers",
+        "powers": ["Flight", "Energy Blasts", "Super Strength"],
+        "team": "Avengers",
+        "home": "Boston"
+    },
+    "Wolverine": {
+        "real_name": "Logan",
+        "powers": ["Regeneration", "Adamantium Claws", "Enhanced Senses"],
+        "team": "X-Men",
+        "home": "Canada"
+    },
+    "Storm": {
+        "real_name": "Ororo Munroe",
+        "powers": ["Weather Control", "Flight", "Lightning"],
+        "team": "X-Men",
+        "home": "Kenya"
+    },
+    "Cyclops": {
+        "real_name": "Scott Summers",
+        "powers": ["Optic Blasts", "Leadership", "Tactics"],
+        "team": "X-Men",
+        "home": "Alaska"
+    },
+    "Jean Grey": {
+        "real_name": "Jean Grey",
+        "powers": ["Telepathy", "Telekinesis", "Phoenix Force"],
+        "team": "X-Men",
+        "home": "New York"
+    },
+    "Rogue": {
+        "real_name": "Anna Marie",
+        "powers": ["Power Absorption", "Flight", "Super Strength"],
+        "team": "X-Men",
+        "home": "Mississippi"
+    },
+    "Nightcrawler": {
+        "real_name": "Kurt Wagner",
+        "powers": ["Teleportation", "Agility", "Night Vision"],
+        "team": "X-Men",
+        "home": "Germany"
+    },
+    "Deadpool": {
+        "real_name": "Wade Wilson",
+        "powers": ["Regeneration", "Swordsmanship", "Humor"],
+        "team": "None",
+        "home": "Canada"
+    },
+    "Daredevil": {
+        "real_name": "Matt Murdock",
+        "powers": ["Enhanced Senses", "Martial Arts", "Acrobatics"],
+        "team": "Defenders",
+        "home": "Hell's Kitchen"
+    },
+    "Luke Cage": {
+        "real_name": "Luke Cage",
+        "powers": ["Super Strength", "Unbreakable Skin", "Stamina"],
+        "team": "Defenders",
+        "home": "Harlem"
+    },
+    "Iron Fist": {
+        "real_name": "Danny Rand",
+        "powers": ["Chi Energy", "Martial Arts", "Healing"],
+        "team": "Defenders",
+        "home": "K'un-Lun"
+    },
+    "Jessica Jones": {
+        "real_name": "Jessica Jones",
+        "powers": ["Super Strength", "Flight", "Durability"],
+        "team": "Defenders",
+        "home": "New York City"
+    },
+    "Green Arrow": {
+        "real_name": "Oliver Queen",
+        "powers": ["Archery", "Martial Arts", "Wealth"],
+        "team": "Justice League",
+        "home": "Star City"
+    },
+    "Shazam": {
+        "real_name": "Billy Batson",
+        "powers": ["Super Strength", "Flight", "Lightning"],
+        "team": "Justice League",
+        "home": "Fawcett City"
+    },
+    "Ant-Man": {
+        "real_name": "Scott Lang",
+        "powers": ["Size Manipulation", "Insect Control", "Strength"],
+        "team": "Avengers",
+        "home": "San Francisco"
+    }
+}
