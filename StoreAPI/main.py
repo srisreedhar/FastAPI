bafb22bf7f7b3a3b188bc20a5a6f51dddbf1b23f
srisreedhar <srisreedhar@live.com>
2025-07-31 19:15:32 +0530

    added all-api-endpoints

--- StoreAPI/main.py
+++ StoreAPI/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from os import environ
 from datetime import datetime,UTC
-
+from typing import Annotated, Optional
 
 
 app = FastAPI()
 
+
+# App Ping - Home Page
 @app.get("/")
 def home():
     return {
@@ -15,3 +17,14 @@ def home():
         "user": environ["USERNAME"],
         "intel-arch": environ["PROCESSOR_ARCHITECTURE"],
     }
+
+# Get all the Endpoints
+@app.get("/list-endpoints")
+def list_endpoints():
+    endpoints = []
+    for each_route in app.routes:
+        endpoint = [{"path": each_route.path, "methods": list(each_route.methods)}]
+        endpoints.append(endpoint)
+    return {
+        "endpoints": endpoints
+        }
